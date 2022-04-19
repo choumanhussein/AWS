f184e2817d5104b5ab797744aa11da87dbea38cf
choumanhussein <chouman.hussein7@gmail.com>
2022-04-19 21:50:58 +0000

    Yarn++++

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,7 +71,7 @@ namespace:deploy do
    task:yarn_install do
       on roles(:web) do
          within release_path do
-            execute("cd #{release_path} && yarn install")
+            execute("cd #{release_path} && yarn install --ignore-engines")
          end
       end
    end
